1fb513e41a207c5bf8df1ff8a176e6a6939e17e6
shadinmd <shadinmhd97@gmail.com>
2025-02-18 15:59:12 +0530

    fix: live grep is not working

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,10 +14,6 @@ return {
 			},
 			defaults = {
 				file_ignore_patterns = { "node_modules", "git", "debug", "Debug" },
-				vimgrep_arguments = {
-					"--no-ignore",
-					"--hidden"
-				}
 			},
 			extensions = {
 				fzf = {
