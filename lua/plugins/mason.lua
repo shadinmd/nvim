4decbdaf92d814f350ed6d9cd05d5577d52ce9c8
shadinmd <shadinmhd97@gmail.com>
2025-05-15 21:45:47 +0530

    update: change config according to new updates

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,29 +2,28 @@ return {
 	"williamboman/mason.nvim",
 	lazy = false,
 	dependencies = {
-		'williamboman/mason-lspconfig.nvim',
-		'neovim/nvim-lspconfig'
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
 	},
 	config = function()
-		require("mason").setup({})
+		require("mason").setup()
+
 		local masonlsp = require("mason-lspconfig")
 		local servers = require("utils").servers
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 		masonlsp.setup({
-			ensure_installed = vim.tbl_keys(servers)
+			ensure_installed = vim.tbl_keys(servers),
 		})
 
-		masonlsp.setup_handlers({
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-					on_attach = require("utils").on_attach,
-					settings = servers[server_name],
-					filetypes = (servers[server_name] or {}).filetypes
-				})
-			end
-		})
-	end
+		for server_name, config in pairs(servers) do
+			require("lspconfig")[server_name].setup({
+				capabilities = capabilities,
+				on_attach = require("utils").on_attach,
+				settings = config,
+				filetypes = config.filetypes,
+			})
+		end
+	end,
 }
