2cfc10b3b738bf1a269044828f940cb690531f4f
shadinmd <shadinmhd97@gmail.com>
2025-10-02 11:38:09 +0530

    update: removed kick start name prefix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,25 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	event = 'VimEnter',
-	tag = '0.1.3',
+	"nvim-telescope/telescope.nvim",
+	event = "VimEnter",
+	tag = "0.1.3",
 	dependencies = {
-		'nvim-lua/plenary.nvim',
-		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
-		{ 'nvim-telescope/telescope-ui-select.nvim' },
+		"nvim-lua/plenary.nvim",
 		{
-			'nvim-tree/nvim-web-devicons',
-			enabled = vim.g.have_nerd_font
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		},
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+		{
+			"nvim-tree/nvim-web-devicons",
+			enabled = vim.g.have_nerd_font,
 		},
 	},
 	config = function()
-		require('telescope').setup {
+		require("telescope").setup({
 			pickers = {
 				find_files = {
-					hidden = true
-				}
+					hidden = true,
+				},
 			},
 			defaults = {
 				file_ignore_patterns = { "node_modules", "git", "debug", "Debug" },
@@ -28,27 +31,26 @@ return {
 					override_file_sorter = true,
 					case_mode = "smart_case",
 				},
-				['ui-select'] = {
-					require('telescope.themes').get_dropdown(),
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
 				},
+			},
+		})
 
-			}
-		}
+		pcall(require("telescope").load_extension, "fzf")
+		pcall(require("telescope").load_extension, "ui-select")
 
-		pcall(require('telescope').load_extension, 'fzf')
-		pcall(require('telescope').load_extension, 'ui-select')
+		local builtin = require("telescope.builtin")
 
-		local builtin = require('telescope.builtin')
-
-		vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>sh', builtin.help_tags, {})
-		vim.keymap.set('n', '<leader>/', function()
-			builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-				winblend = 10,
-				previewer = false,
-			})
-		end, { desc = '[/] Fuzzily search in current buffer' })
-	end
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
+		vim.keymap.set("n", "<leader>/", function()
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 0,
+				previewer = true,
+			}))
+		end, { desc = "[/] Fuzzily search in current buffer" })
+	end,
 }
