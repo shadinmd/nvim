04fc21b22002e16251fb05ff74f8fb239b20f8df
shadinmd <shadinmhd97@gmail.com>
2025-10-02 11:37:46 +0530

    update: changed auto cmd location

--- init.lua
+++ init.lua
@@ -1,12 +1,4 @@
 require("options")
 require("keymap")
 require("lazyvim")
-
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.hl.on_yank()
-	end,
-})
+require("autocmds")
