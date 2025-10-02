0f0752e917c76e62a7c2fde7df3ba4aafd5e22d5
shadinmd <shadinmhd97@gmail.com>
2025-10-02 16:36:28 +0530

    update: added formatter for js project

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -30,11 +30,11 @@ return {
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
-			-- Conform can also run multiple formatters sequentially
 			python = { "black" },
-			--
-			-- You can use 'stop_after_first' to run the first available formatter from the list
-			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
 		},
 	},
 }
