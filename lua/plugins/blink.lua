00978f7a70ea4382742a91f4e9a4f6d2518d91e8
shadinmd <shadinmhd97@gmail.com>
2025-10-02 11:25:52 +0530

    untracked changes

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,55 +1,56 @@
 return {
-		'saghen/blink.cmp',
-		event = 'VimEnter',
-		version = '1.*',
-		dependencies = {
-			{
-				'L3MON4D3/LuaSnip',
-				version = '2.*',
-				build = (function()
-					if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-						return
-					end
-					return 'make install_jsregexp'
-				end)(),
-				dependencies = {
-					{
-						'rafamadriz/friendly-snippets',
-						config = function()
-							require('luasnip.loaders.from_vscode').lazy_load()
-						end,
-					},
-				},
-				opts = {},
-			},
-			'folke/lazydev.nvim',
-		},
-		--- @module 'blink.cmp'
-		--- @type blink.cmp.Config
-		opts = {
-			keymap = {
-				preset = 'default',
-			},
-
-			appearance = {
-				nerd_font_variant = 'mono',
-			},
-
-			completion = {
-				-- By default, you may press `<c-space>` to show the documentation.
-				-- Optionally, set `auto_show = true` to show the documentation after a delay.
-				documentation = { auto_show = false, auto_show_delay_ms = 500 },
-			},
-
-			sources = {
-				default = { 'lsp', 'path', 'snippets', 'lazydev' },
-				providers = {
-					lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+	"saghen/blink.cmp",
+	event = "VimEnter",
+	version = "1.*",
+	dependencies = {
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
 				},
 			},
-
-			snippets = { preset = 'luasnip' },
-			fuzzy = { implementation = 'lua' },
-			signature = { enabled = true },
+			opts = {},
 		},
-	}
+		"folke/lazydev.nvim",
+	},
+	--- @module 'blink.cmp'
+	--- @type blink.cmp.Config
+	opts = {
+		keymap = {
+			preset = "default",
+			["<CR>"] = { "select_and_accept" },
+		},
+
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+
+		completion = {
+			-- By default, you may press `<c-space>` to show the documentation.
+			-- Optionally, set `auto_show = true` to show the documentation after a delay.
+			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+		},
+
+		sources = {
+			default = { "lsp", "path", "snippets", "lazydev" },
+			providers = {
+				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+			},
+		},
+
+		snippets = { preset = "luasnip" },
+		fuzzy = { implementation = "lua" },
+		signature = { enabled = true },
+	},
+}
