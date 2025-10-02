d105c4e33ce2de381deb9c2cad6356dde8f5fd30
shadinmd <shadinmhd97@gmail.com>
2025-10-02 22:20:47 +0530

    update: removed auto show documentation delay in completion menu

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -49,7 +49,7 @@ return {
 			preselect = "first",
 			-- By default, you may press `<c-space>` to show the documentation.
 			-- Optionally, set `auto_show = true` to show the documentation after a delay.
-			documentation = { auto_show = true, auto_show_delay_ms = 500 },
+			documentation = { auto_show = true, auto_show_delay_ms = 0 },
 		},
 
 		sources = {
