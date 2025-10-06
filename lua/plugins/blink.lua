edc5c47e3a5ec9d82bff233c364ed37c6bc2a337
shadinmd <shadinmhd97@gmail.com>
2025-10-06 15:13:39 +0000

    fix: preselect first completion on list

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -46,12 +46,16 @@ return {
 		},
 
 		completion = {
-			preselect = "first",
 			-- By default, you may press `<c-space>` to show the documentation.
 			-- Optionally, set `auto_show = true` to show the documentation after a delay.
 			documentation = { auto_show = true, auto_show_delay_ms = 0 },
 			accept = {
 				auto_brackets = { enabled = false },
+			},
+			list = {
+				selection = {
+					preselect = true,
+				},
 			},
 		},
 
