c65ebe171b214ecef29ced2018de97b5c6e58dcb
shadin muhammed <shadin@shadins-MacBook-Pro.local>
2026-02-12 11:48:23 +0530

    chore: add default comment.nvim config

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,29 @@
 return {
 	"numToStr/Comment.nvim",
-	opts = {},
-	lazy = false,
+	event = "VeryLazy",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		local ts_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+
+		require("Comment").setup({
+			pre_hook = function(ctx)
+				local ft = vim.bo.filetype
+
+				local no_comment = {
+					json = true,
+					jsonc = true,
+					dotenv = true,
+					env = true,
+				}
+
+				if no_comment[ft] then
+					return ""
+				end
+
+				return ts_hook(ctx)
+			end,
+		})
+	end,
 }
