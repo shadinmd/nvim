c7637a7dd1d14434ccb1b58f4aa4706efbe1ca5c
shadin <shadinmhd97@gmail.com>
2026-04-01 23:55:48 +0530

    fix: treesitter not working with filetype typescriptreact, javascriptreact

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,4 +18,16 @@ return {
 		highlight = { enable = true },
 		indent = { enable = true },
 	},
+
+	config = function(_, opts)
+		-- require("nvim-treesitter").setup(opts)
+
+		-- vim.treesitter.language.register("tsx", "typescriptreact")
+		-- vim.treesitter.language.register("tsx", "javascriptreact")
+		vim.api.nvim_create_autocmd("BufReadPost", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+	end,
 }
